23ac0134705dad88ba7c4dd2df0f91d593498b6e
unknown <mjcousineau1@gmail.com>
2021-07-16 14:07:36 -0400

    Refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.security.DigestInputStream;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import com.twmacinta.util.MD5;
 
 public class Main {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
 		// TODO Auto-generated method stub
-	    PrintWriter writer = new PrintWriter("testfile.txt", "UTF-8");
+	    PrintWriter writer = new PrintWriter("aipatchfile.txt", "UTF-8");
 		scanLocalDirectory(writer, System.getProperty("user.dir"), System.getProperty("user.dir"));
 	    writer.close();
 	}
@@ -22,9 +19,9 @@ public class Main {
 	    File[] fileList = directory.listFiles();
 	    if(fileList != null) {
 	        for (File file : fileList) {      
-	            if (file.isFile()) {
+	            if (!file.getName().contains("patchadams.jar") && file.isFile()) {
 	                System.out.println("processLocalPatchFile: Adding file path to fileList: " + file.getPath());
-		        	    writer.println(file.getPath().replace(baseDirectory, "") + "$" + createFileChecksum(file.getAbsolutePath()));  //$ to split on
+		        	writer.println(file.getPath().replace(baseDirectory, "") + "$" + createFileChecksumAlt(file.getAbsolutePath()));  //$ to split filename/checksum on
 	            } else if (file.isDirectory()) {
 	            	scanLocalDirectory(writer, file.getAbsolutePath(), baseDirectory);
 	            }
@@ -32,19 +29,8 @@ public class Main {
 	    }
 	}
 	
-	public static String createFileChecksum(String filePath) throws NoSuchAlgorithmException, FileNotFoundException, IOException {
-        MessageDigest md = MessageDigest.getInstance("SHA-256"); 
-        // file hashing with DigestInputStream
-        try (DigestInputStream dis = new DigestInputStream(new FileInputStream(filePath), md)) {
-            while (dis.read() != -1) ; //empty loop to clear the data
-            md = dis.getMessageDigest();
-        }
-
-        // bytes to hex
-        StringBuilder result = new StringBuilder();
-        for (byte b : md.digest()) {
-            result.append(String.format("%02x", b));
-        }
-        return result.toString();
+	public static String createFileChecksumAlt(String filePath) throws IOException {
+		String hash = MD5.asHex(MD5.getHash(new File(filePath)));
+		return hash;
 	}
 }
